eed3c841b9bca73241e9f62fe8e0a81d7488372f
eceturgut <ece.turgut@ogr.sakarya.edu.tr>
2026-05-04 14:19:32 +0300

    Tüm sayfalara navbar tanımlama

---
+++ ilgi-alanlarim.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>İlgi Alanlarım | Web Teknolojileri Projesi</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">Ece Turgut</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="index.php">Hakkımda</a></li>
+                    <li class="nav-item"><a class="nav-link" href="cv.php">Özgeçmişim</a></li>
+                    <li class="nav-item"><a class="nav-link" href="sehrim.php">Şehrim</a></li>
+                    <li class="nav-item"><a class="nav-link" href="mirasim.php">Mirasım</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="ilgi-alanlarim.php">İlgi Alanlarım</a></li>
+                    <li class="nav-item"><a class="nav-link" href="iletisim.php">İletişim</a></li>
+                    <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
